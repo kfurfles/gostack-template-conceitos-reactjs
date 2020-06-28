5159912183ee6ee9cc0f3bd67e769568c62c391f
furflesx <kelvinsilva.ksv@gmail.com>
2020-06-28 17:52:53 -0300

    implementacao final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getRepositories, deleteRepositories, createRepo } from "./services/api"
 
 import "./styles.css";
 
 function App() {
+  const [repos,setRepos] =  useState([])
+
   async function handleAddRepository() {
-    // TODO
+    const newRepo = await createRepo()
+    setRepos([...repos, newRepo])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await deleteRepositories(id)
+      setRepos(removeItemById(id))      
+    } catch (error) {
+      console.error(error)
+    }
   }
+
+  
+  function removeItemById(id){
+    return repos.filter(repo => repo.id !== id)
+  }
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      const repoList = await getRepositories()
+      setRepos(repoList)
+    }
+    fetchRepos()
+  },[])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repos.map(repos => {
+          return <li key={repos.id}> 
+            {repos.title}
+            <button onClick={() => handleRemoveRepository(repos.id)}>
+              Remover
+            </button>
+          </li>
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,33 @@
 import axios from "axios";
+import faker from 'faker'
 
 const api = axios.create({
   baseURL: "http://localhost:3333",
 });
+
+export async function getRepositories(){
+  const { data } = await api.get(`/repositories`)
+  return data
+}
+
+export async function deleteRepositories(id){
+  const response = await api.delete(`/repositories/${id}`)
+  return response
+}
+
+export async function createRepo(){
+  const result = {
+    "title": faker.name.firstName(),
+    "url": faker.internet.url(),
+    "techs": [
+      faker.internet.avatar(),
+      faker.internet.avatar(),
+      faker.internet.avatar()
+    ]
+  }
+
+  const { data } = await api.post(`/repositories`,result)
+  return data
+}
 
 export default api;
